ee09d5167efc4fd4bd493f74e87ef7b2928fbc5a
remittor <remittor@gmail.com>
2016-01-28 17:06:49 +0300

    Adapt RILC for support legacy RILQ blobs on Android 6.0
    Delete unsupported and deprecated solicited requests:
        RIL_REQUEST_GET_DATA_CALL_PROFILE
        RIL_REQUEST_SET_DATA_SUBSCRIPTION
        RIL_REQUEST_GET_UICC_SUBSCRIPTION
        RIL_REQUEST_GET_DATA_SUBSCRIPTION
        RIL_REQUEST_SET_SUBSCRIPTION_MODE
        RIL_REQUEST_SIM_TRANSMIT_BASIC
        RIL_REQUEST_SIM_OPEN_CHANNEL
        RIL_REQUEST_SIM_CLOSE_CHANNEL
        RIL_REQUEST_SIM_TRANSMIT_CHANNEL
        RIL_REQUEST_SIM_GET_ATR

--- ril/ril-legacy/libril/rilj.h
+++ ril/ril-legacy/libril/rilj.h
@@ -5,28 +5,28 @@
 
 #define J_RIL_REQUEST_IMS_REGISTRATION_STATE              112
 #define J_RIL_REQUEST_IMS_SEND_SMS                        113
-#define J_RIL_REQUEST_GET_DATA_CALL_PROFILE               114
+#define J_RIL_REQUEST_GET_DATA_CALL_PROFILE             11114
 
-#define J_RIL_REQUEST_SET_UICC_SUBSCRIPTION               115
-#define J_RIL_REQUEST_SET_DATA_SUBSCRIPTION               116
+#define J_RIL_REQUEST_SET_UICC_SUBSCRIPTION               122
+#define J_RIL_REQUEST_SET_DATA_SUBSCRIPTION             10119
 #define J_RIL_REQUEST_GET_UICC_SUBSCRIPTION             10120
 #define J_RIL_REQUEST_GET_DATA_SUBSCRIPTION             10121
 #define J_RIL_REQUEST_SET_SUBSCRIPTION_MODE             10122
 
-#define J_RIL_REQUEST_SIM_TRANSMIT_BASIC                  117
-#define J_RIL_REQUEST_SIM_OPEN_CHANNEL                    118
-#define J_RIL_REQUEST_SIM_CLOSE_CHANNEL                   119
-#define J_RIL_REQUEST_SIM_TRANSMIT_CHANNEL                120
-#define J_RIL_REQUEST_SIM_GET_ATR                         121
+#define J_RIL_REQUEST_SIM_TRANSMIT_APDU_BASIC             114
+#define J_RIL_REQUEST_SIM_OPEN_CHANNEL                    115
+#define J_RIL_REQUEST_SIM_CLOSE_CHANNEL                   116
+#define J_RIL_REQUEST_SIM_TRANSMIT_APDU_CHANNEL           117
+#define J_RIL_REQUEST_SIM_GET_ATR                         136
 
-#define J_RIL_REQUEST_MAX                                 121
+#define J_RIL_REQUEST_MAX                                 136
 
 
 #define J_RIL_UNSOL_VOICE_RADIO_TECH_CHANGED              1035
 #define J_RIL_UNSOL_CELL_INFO_LIST                        1036
 #define J_RIL_UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED    1037
-#define J_RIL_UNSOL_ON_SS                                 1038
-#define J_RIL_UNSOL_STK_CC_ALPHA_NOTIFY                   1039
-#define J_RIL_UNSOL_UICC_SUBSCRIPTION_STATUS_CHANGED      1040
+#define J_RIL_UNSOL_ON_SS                                 1043
+#define J_RIL_UNSOL_STK_CC_ALPHA_NOTIFY                   1044
+#define J_RIL_UNSOL_UICC_SUBSCRIPTION_STATUS_CHANGED      1038
 
-#define J_RIL_UNSOL_MAX                                   1040
+#define J_RIL_UNSOL_MAX                                   1045
