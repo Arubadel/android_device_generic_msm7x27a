579a0c93c50ebbbe4917b1674cf0dad904bdfc35
grace5921 <androidlover5842@gmail.com>
2017-05-17 09:02:38 +0530

    msm7x27a: work around key lights issue

--- lights/lights.c
+++ lights/lights.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2011 Kolja Dummann <k.dummann@gmail.com>
+ * Copyright (C) 2008 The Android Open Source Project.
+ * Copyright (C) 2014 The CyanogenMod Project.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,24 +15,39 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "lights.msm7x27a"
 
-#define LOG_NDEBUG 1
+// #define LOG_NDEBUG 0
+#define LOG_TAG "lights.msm7627a"
 
 #include <cutils/log.h>
+
 #include <stdint.h>
 #include <string.h>
+#include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <malloc.h>
 #include <pthread.h>
+
 #include <sys/ioctl.h>
 #include <sys/types.h>
+
 #include <hardware/lights.h>
+
+/******************************************************************************/
+
 static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
-char const *const LCD_FILE = "/sys/class/leds/lcd-backlight/brightness";
-char const *const BUTTON_FILE = " /sys/class/sec/sec_touchkey/brightness";
+char const*const LCD_FILE
+        = "/sys/class/leds/lcd-backlight/brightness";
+
+char const*const BUTTON_FILE
+        = "/sys/class/sec/sec_touchkey/brightness";
+
+/**
+ * device methods
+ */
 
 void init_globals(void)
 {
@@ -39,20 +55,17 @@ void init_globals(void)
     pthread_mutex_init(&g_lock, NULL);
 }
 
-static int write_int(char const *path, int value)
+static int
+write_int(char const* path, int value)
 {
     int fd;
-    static int already_warned;
+    static int already_warned = 0;
 
-    already_warned = 0;
-
-    ALOGV("write_int: path %s, value %d", path, value);
     fd = open(path, O_RDWR);
-
     if (fd >= 0) {
         char buffer[20];
-        int bytes = sprintf(buffer, "%d\n", value);
-        int amt = write(fd, buffer, bytes);
+        int bytes = snprintf(buffer, sizeof(buffer), "%d\n", value);
+        ssize_t amt = write(fd, buffer, (size_t)bytes);
         close(fd);
         return amt == -1 ? -errno : 0;
     } else {
@@ -64,27 +77,35 @@ static int write_int(char const *path, int value)
     }
 }
 
-static int rgb_to_brightness(struct light_state_t const *state)
+static int
+is_lit(struct light_state_t const* state)
 {
-     int color = state->color & 0x00ffffff;
-
-     return ((77*((color>>16) & 0x00ff))
-		+ (150*((color>>8) & 0x00ff)) + (29*(color & 0x00ff))) >> 8;
+    return state->color & 0x00ffffff;
 }
 
-static int is_lit(struct light_state_t const* state)
+static int
+rgb_to_brightness(struct light_state_t const* state)
 {
-     return state->color & 0x00ffffff;
+    int color = state->color & 0x00ffffff;
+    return ((77*((color>>16)&0x00ff))
+            + (150*((color>>8)&0x00ff)) + (29*(color&0x00ff))) >> 8;
 }
 
-static int set_light_backlight(struct light_device_t *dev, struct light_state_t const* state)
+static int
+set_light_backlight(struct light_device_t *dev,
+        struct light_state_t const* state)
 {
-     int err = 0;
-     int brightness = rgb_to_brightness(state);
-     pthread_mutex_lock(&g_lock);
-     err = write_int(LCD_FILE, brightness);
-     pthread_mutex_unlock(&g_lock);
-     return err;
+    int err = 0;
+    int brightness = rgb_to_brightness(state);
+
+    if(!dev) {
+        return -1;
+    }
+
+    pthread_mutex_lock(&g_lock);
+    err = write_int(LCD_FILE, brightness);
+    pthread_mutex_unlock(&g_lock);
+    return err;
 }
 
 static int set_light_buttons (struct light_device_t* dev, struct light_state_t const* state) {
@@ -98,58 +119,71 @@ static int set_light_buttons (struct light_device_t* dev, struct light_state_t c
          write_int(BUTTON_FILE, 0);
 
      pthread_mutex_unlock (&g_lock);
-     return 0;
+     return err;
 }
 
-static int close_lights(struct light_device_t *dev)
+/** Close the lights device */
+static int
+close_lights(struct light_device_t *dev)
 {
-     ALOGV("close_light is called");
-     if (dev)
-         free(dev);
-
-     return 0;
+    if (dev) {
+        free(dev);
+    }
+    return 0;
 }
 
-static int open_lights(const struct hw_module_t *module, char const *name, struct hw_device_t **device)
+/******************************************************************************/
+
+/**
+ * module methods
+ */
+
+/** Open a new instance of a lights device using name */
+static int open_lights(const struct hw_module_t* module, char const* name,
+        struct hw_device_t** device)
 {
-     int (*set_light)(struct light_device_t *dev,
-		struct light_state_t const *state);
+    int (*set_light)(struct light_device_t *dev,
+            struct light_state_t const* state);
 
-     ALOGV("open_lights: open with %s", name);
-
-     if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
+    if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
         set_light = set_light_backlight;
-     else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
+    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
         set_light = set_light_buttons;
-     else
+    else
         return -EINVAL;
 
-     pthread_once(&g_init, init_globals);
+    pthread_once(&g_init, init_globals);
 
-     struct light_device_t *dev = malloc(sizeof(struct light_device_t));
-     memset(dev, 0, sizeof(*dev));
+    struct light_device_t *dev = malloc(sizeof(struct light_device_t));
 
-     dev->common.tag = HARDWARE_DEVICE_TAG;
-     dev->common.version = 0;
-     dev->common.module = (struct hw_module_t *)module;
-     dev->common.close = (int (*)(struct hw_device_t *))close_lights;
-     dev->set_light = set_light;
+    if(!dev)
+        return -ENOMEM;
 
-     *device = (struct hw_device_t *)dev;
+    memset(dev, 0, sizeof(*dev));
 
-     return 0;
+    dev->common.tag = HARDWARE_DEVICE_TAG;
+    dev->common.version = 0;
+    dev->common.module = (struct hw_module_t*)module;
+    dev->common.close = (int (*)(struct hw_device_t*))close_lights;
+    dev->set_light = set_light;
+
+    *device = (struct hw_device_t*)dev;
+    return 0;
 }
 
 static struct hw_module_methods_t lights_module_methods = {
-     .open =  open_lights,
+    .open = open_lights,
 };
 
+/*
+ * The lights Module
+ */
 struct hw_module_t HAL_MODULE_INFO_SYM = {
-     tag: HARDWARE_MODULE_TAG,
-     module_api_version: 1,
-     hal_api_version: 0,
-     id: LIGHTS_HARDWARE_MODULE_ID,
-     name: "lights Module",
-     author: "Kolja Dummann <k.dummann@gmail.com>",
-     methods: &lights_module_methods,
+    .tag = HARDWARE_MODULE_TAG,
+    .version_major = 1,
+    .version_minor = 0,
+    .id = LIGHTS_HARDWARE_MODULE_ID,
+    .name = "msm7627a Lights Module",
+    .author = "The CyanogenMod Project",
+    .methods = &lights_module_methods,
 };
