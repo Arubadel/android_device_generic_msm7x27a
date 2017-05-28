0161a7086be1cee626612ce105884d3ba4b03018
androidlover5842 <androidlover5842@gmail.com>
2017-05-28 20:56:46 +0530

    gps: clean gps 50001 codes

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -1,13 +1,7 @@
-ifneq ($(TARGET_PROVIDES_GPS_LOC_API),true)
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-ifneq ($(BUILD_TINY_ANDROID),true)
-#Compile this library only for builds with the latest modem image
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-## Libs
 LOCAL_SHARED_LIBRARIES := \
     libutils \
     libcutils
@@ -21,9 +15,6 @@ LOCAL_SRC_FILES += \
 LOCAL_CFLAGS += \
      -fno-short-enums \
      -D_ANDROID_
-
-## Includes
-LOCAL_C_INCLUDES:=
 
 LOCAL_COPY_HEADERS_TO:= gps.utils/
 LOCAL_COPY_HEADERS:= \
@@ -41,6 +32,3 @@ LOCAL_PRELINK_MODULE := false
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 include $(BUILD_SHARED_LIBRARY)
-endif # not BUILD_TINY_ANDROID
-endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
-endif#TARGET_PROVIDES_GPS_LOC_API
